9f5ecaa18d3a6fc9c2374869269a105b29f6278d
Vedansh Mittal <56213441+VedanshM@users.noreply.github.com>
2021-11-16 19:46:06 +0530

    basic UI, not working properly

---
+++ grabcut_opencv.py
@@ -0,0 +1,170 @@
+import numpy as np
+import types
+import cv2 as cv
+
+###### Constants ##########
+COLORS = types.SimpleNamespace(
+    red=[255, 0, 0],
+    green=[0, 255, 0],
+    blue=[0, 0, 255],
+    black=[0, 0, 0],
+    white=[255, 255, 255]
+)
+
+BRUSH_MODES = {
+    "bg": {"col": COLORS.black, "mask_val": 0},
+    "fg": {"col": COLORS.white, "mask_val": 1},
+    "pr_bg": {"col": COLORS.red, "mask_val": 2},
+    "pr_fg": {"col": COLORS.green, "mask_val": 3},
+}
+###########################
+
+
+class WindowManager:
+    INPUT_WINDOW = 'input'
+    OUTPUT_WINDOW = 'output'
+
+    def __init__(self, img_path: str, grabcut_fn=cv.grabCut):
+        self.grabcut_fn = grabcut_fn
+        self.org_img = cv.imread(img_path)
+        self.disp_img = self.org_img.copy()
+        self.out_img = np.zeros(self.org_img.shape, np.uint8)
+        self.mask = np.zeros(self.org_img.shape, np.uint8)
+        cv.namedWindow(self.INPUT_WINDOW, cv.WINDOW_AUTOSIZE |
+                       cv.WINDOW_KEEPRATIO | cv.WINDOW_GUI_NORMAL)
+        cv.namedWindow(self.OUTPUT_WINDOW, cv.WINDOW_AUTOSIZE |
+                       cv.WINDOW_KEEPRATIO | cv.WINDOW_GUI_NORMAL)
+        cv.setMouseCallback(self.INPUT_WINDOW, self.mouse_callback)
+        cv.moveWindow(self.INPUT_WINDOW, self.org_img.shape[1]+10, 90)
+        self.reset_state()
+
+    def run(self):
+        # print(hash(self.disp_img.tobytes()))
+
+        self.draw_ns.brush_mode = BRUSH_MODES["bg"]
+        key_to_mode = {
+            '0': BRUSH_MODES["bg"],
+            "1": BRUSH_MODES["fg"],
+            "2": BRUSH_MODES["pr_bg"],
+            "3": BRUSH_MODES["pr_fg"],
+        }
+
+        key_to_act = {
+            "s": self.save_output,
+            "r": self.reset_state,
+            "n": self.perform_segmentation,
+            "b": self.toggle_rect_draw_mode,
+        }
+
+        while True:
+            cv.imshow(self.INPUT_WINDOW, self.disp_img)
+            hard_mask = (self.mask == 1) | (self.mask == 3)
+            self.out_img = self.org_img*hard_mask
+
+            cv.imshow(self.OUTPUT_WINDOW, self.out_img)
+            k = cv.waitKey(1)
+            if k < 0:
+                continue
+
+            k = chr(k)
+
+            if k in key_to_mode:
+                self.draw_ns.brush_mode = key_to_mode[k]
+            if k in key_to_act:
+                key_to_act[k]()
+
+    def perform_segmentation(self):
+        #updates the mask to corresponding segmentation
+        print(self.init_mode)
+        tmp1 = np.zeros((1, 65), np.float64)
+        tmp2 = tmp1.copy()
+        print(self.rect.shape)
+
+        self.grabcut_fn(self.disp_img,
+                        self.mask,
+                        self.rect,
+                        tmp1,
+                        tmp2,
+                        1,
+                        self.init_mode
+                        )
+        self.init_mode = False
+
+    def toggle_rect_draw_mode(self):
+        self.draw_ns.rect_on = not self.draw_ns.rect_on
+
+    def save_output(self, file_path: str = "out.png"):
+        cv.imwrite(file_path, self.out_img)
+        print("Output image saved at ", file_path)
+
+    def reset_state(self):
+        self.rect = (0, 0, 1, 1)
+        self.init_mode = cv.GC_INIT_WITH_RECT
+        self.drawing = True
+        self.rectangle = False
+        self.draw_ns = types.SimpleNamespace()
+        self.draw_ns.rect_on = False
+        self.draw_ns.brush_on = False
+        self.draw_ns.ix = 0
+        self.draw_ns.iy = 0
+        self.draw_ns.brush_sz = 3
+
+    def mouse_callback(self, event, x, y, flags, param):
+        def mark_circle():
+            cv.circle(self.disp_img, (x, y), self.draw_ns.brush_sz,
+                      self.draw_ns.brush_mode['col'], -1)
+            cv.circle(self.mask, (x, y), self.draw_ns.brush_sz,
+                      self.draw_ns.brush_mode['mask_val'], -1)
+
+        def mark_rectangle(set_rect=False):
+            ix, iy = self.draw_ns.ix, self.draw_ns.iy
+            print((ix, iy), (x, y))
+            img = self.org_img.copy()
+            cv.rectangle(img, (ix, iy), (x, y), COLORS.blue, 2)
+            self.disp_img = img
+
+            if set_rect:
+                # left, up, width, height
+                self.rect = (min(ix, x), min(iy, y), abs(ix-x), abs(iy-y))
+
+        if event == cv.EVENT_RBUTTONDOWN:
+            print("rdown")
+            self.draw_ns.rect_on = True
+            self.draw_ns.ix, self.draw_ns.iy = x, y
+            self.init_mode = cv.GC_INIT_WITH_RECT
+
+        elif event == cv.EVENT_RBUTTONUP:
+            print("rup")
+            if self.draw_ns.rect_on:
+                self.draw_ns.rect_on = False
+                mark_rectangle(set_rect=True)
+                self.init_mode = cv.GC_INIT_WITH_RECT
+
+        elif event == cv.EVENT_MOUSEMOVE:
+            if self.draw_ns.rect_on:
+                print("m rect")
+                mark_rectangle()
+
+            elif self.draw_ns.brush_on:
+                mark_circle()
+
+        elif event == cv.EVENT_LBUTTONDOWN:
+            if self.draw_ns.rect_on:
+                print("Draw the Rect first")
+                return
+            self.draw_ns.brush_on = True
+            mark_circle()
+
+        elif event == cv.EVENT_LBUTTONUP:
+            if self.draw_ns.rect_on:
+                print("Draw the Rect first")
+                return
+            if self.draw_ns.brush_on:
+                self.draw_ns.brush_on = False
+                mark_circle()
+                self.init_mode = cv.GC_INIT_WITH_MASK
+
+
+if __name__ == '__main__':
+    wm = WindowManager("messi5.jpg")
+    wm.run()
