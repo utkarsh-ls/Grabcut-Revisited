47c7711c8113d9f5c9d524917e4c8d5cc59c5e34
BhaskarJoshi-01 <bhaskar.joshi@research.iiit.ac.in>
2021-11-24 20:13:18 +0530

    grabcut function done

--- grabcut.py
+++ grabcut.py
@@ -2,6 +2,7 @@ from typing import Tuple
 import numpy as np
 import igraph as ig
 from gmm import GMM
+import types
 
 MASK_VALUE = {
 
@@ -11,227 +12,248 @@ MASK_VALUE = {
     "pr_fg": 3,
 }
 
-class GrabCut:
-    def __init__(self, img, mask, rect=None, gmm_components=5):
-        self.img = np.asarray(img, dtype=np.float64)
-        self.rows, self.cols, _ = img.shape
+# These are the best no of GMM components K
+# that were mentioned in paper
+gmm_components = 5
+gamma = 50
+beta = 0
 
-        self.mask = mask
-        # print("debug",self.mask.sum(),self.mask.std())
-        if rect is not None:
-            self.mask[rect[1]:rect[1] + rect[3],
-                      rect[0]:rect[0] + rect[2]] = 3
-        self.classify_pixels()
 
-        # Best number of GMM components K suggested in paper
-        self.gmm_components = gmm_components
-        self.gamma = 50  # Best gamma suggested in paper formula (5)
-        self.beta = 0
 
-        self.left_V = np.empty((self.rows, self.cols - 1))
-        self.upleft_V = np.empty((self.rows - 1, self.cols - 1))
-        self.up_V = np.empty((self.rows - 1, self.cols))
-        self.upright_V = np.empty((self.rows - 1, self.cols - 1))
+def GrabCut(img, init_mask, rect=None):
+    # If we don’t define dtype in the syntax, it is defined by default inferred from the input data.
+    img = np.asarray(img, dtype=np.float64)
+    rows = img.shape[0]
+    cols = img.shape[1]
+    # _=img.shape[2]
+    mask = init_mask
+    if rect is None:
+        pass
+    else:
+        mask[rect[1]:rect[1] + rect[3], rect[0]:rect[0] + rect[2]] = MASK_VALUE['pr_fg']
+    bgd_indexes, fgd_indexes = classify_pixel(mask)
 
-        self.bgd_gmm = None
-        self.fgd_gmm = None
-        self.comp_idxs = np.empty((self.rows, self.cols), dtype=np.uint32)
+    print("bg count = ", bgd_indexes[0].size, end="")
+    print("fg count = ", fgd_indexes[0].size)
+    bgd_gmm, fgd_gmm = None, None
+    smootness = {}
+    smootness["left_V"] = make_empty_array(rows, cols-1)
+    smootness["up_V"] = make_empty_array(rows-1, cols)
+    smootness["upleft_V"] = make_empty_array(rows-1, cols-1)
+    smootness["upright_V"] = make_empty_array(rows-1, cols-1)
+    comp_indxs = make_empty_array(rows, cols).astype('uint32')
+    gc={}
+    gc["graph"]=None
+    gc["graph_capacity"] = None  # Edge capacities
+    gc["source"] = rows*cols    # "object" terminal S
+    gc["sink"] = gc["source"]+1    # "background" terminal T
+    smoothness = calc_beta_smoothness(img, rows, cols, smootness)
 
-        self.gc_graph = None
-        self.gc_graph_capacity = None           # Edge capacities
-        self.gc_source = self.cols * self.rows  # "object" terminal S
-        self.gc_sink = self.gc_source + 1       # "background" terminal T
+    # intitializing gmm
+    bgd_gmm = GMM(img[bgd_indexes])
+    fgd_gmm = GMM(img[fgd_indexes])
 
-        self.calc_beta_smoothness()
-        self.init_GMMs()
-        self.run()
+    #RUN wala karna hai ab ....
+    # RUN part
+    #for number of grabcut iterations
+    ITERATION_CNT=1
+    for _ in range(ITERATION_CNT):
+        # Assigning GMM Components -> Ist step
+        # This is the first part of the 3 steps of paper
+        # It basically assigns GMM components to pixels 
+        comp_indxs[bgd_indexes]=bgd_gmm.component(img[bgd_indexes])
+        comp_indxs[fgd_indexes]=fgd_gmm.component(img[fgd_indexes])
 
-    def calc_beta_smoothness(self):
-        _left_diff = self.img[:, 1:] - self.img[:, :-1]
-        _upleft_diff = self.img[1:, 1:] - self.img[:-1, :-1]
-        _up_diff = self.img[1:, :] - self.img[:-1, :]
-        _upright_diff = self.img[1:, :-1] - self.img[:-1, 1:]
 
-        self.beta = np.sum(np.square(_left_diff)) + np.sum(np.square(_upleft_diff)) + \
-            np.sum(np.square(_up_diff)) + \
-            np.sum(np.square(_upright_diff))
-        self.beta = 1 / (2 * self.beta / (
-            # Each pixel has 4 neighbors (left, upleft, up, upright)
-            4 * self.cols * self.rows
-            # The 1st column doesn't have left, upleft and the last column doesn't have upright
-            - 3 * self.cols
-            - 3 * self.rows  # The first row doesn't have upleft, up and upright
-            + 2))  # The first and last pixels in the 1st row are removed twice
-        print('Beta:', self.beta)
+        # This function performs the second step of image 3 in paper -> Step 2
+        # This learn GMM Parameters from data z 
+        bgd_gmm.fit(img[bgd_indexes],comp_indxs[bgd_indexes])
+        fgd_gmm.fit(img[fgd_indexes],comp_indxs[fgd_indexes])
+        updated_var=construct_gc_graph(mask,gc,bgd_gmm,fgd_gmm,rows,cols,smoothness,img)
+        mask=updated_var["mask"]
+        gc=updated_var["gc"]
+        bgd_gmm=updated_var["bgd_gmm"]
+        fgd_gmm=updated_var["fgd_gmm"]
+        rows=updated_var["rows"]
+        cols=updated_var["cols"]
+        smoothness=updated_var["smoothness"]
+        img=updated_var["img"]
 
-        # Smoothness term V described in formula (11)
-        self.left_V = self.gamma * np.exp(-self.beta * np.sum(
-            np.square(_left_diff), axis=2))
-        self.upleft_V = self.gamma / np.sqrt(2) * np.exp(-self.beta * np.sum(
-            np.square(_upleft_diff), axis=2))
-        self.up_V = self.gamma * np.exp(-self.beta * np.sum(
-            np.square(_up_diff), axis=2))
-        self.upright_V = self.gamma / np.sqrt(2) * np.exp(-self.beta * np.sum(
-            np.square(_upright_diff), axis=2))
 
-    def classify_pixels(self):
-        self.bgd_indexes = np.where(np.logical_or(
-            self.mask == MASK_VALUE['bg'], self.mask == MASK_VALUE['pr_bg']))
-        self.fgd_indexes = np.where(np.logical_or(
-            self.mask == MASK_VALUE['fg'], self.mask == MASK_VALUE['pr_fg']))
 
-        assert self.bgd_indexes[0].size > 0
-        assert self.fgd_indexes[0].size > 0
+    
 
-        print('(pr_)bgd count: %d, (pr_)fgd count: %d' % (
-            self.bgd_indexes[0].size, self.fgd_indexes[0].size))
+def classify_pixel(mask):
+    bgd_indexes = np.where(np.logical_or(
+        mask == MASK_VALUE['bg'], mask == MASK_VALUE['pr_bg']))
+    fgd_indexes = np.where(np.logical_or(
+        mask == MASK_VALUE['fg'], mask == MASK_VALUE['pr_fg']))
 
-    def init_GMMs(self):
-        self.bgd_gmm = GMM(self.img[self.bgd_indexes])
-        self.fgd_gmm = GMM(self.img[self.fgd_indexes])
+    if bgd_indexes[0].size <= 0:
+        print("Incorrect value : error in bg indexes")
 
-    def assign_GMMs_components(self):
-        """Step 1 in Figure 3: Assign GMM components to pixels"""
-        self.comp_idxs[self.bgd_indexes] = self.bgd_gmm.component(
-            self.img[self.bgd_indexes])
-        self.comp_idxs[self.fgd_indexes] = self.fgd_gmm.component(
-            self.img[self.fgd_indexes])
+    if fgd_indexes[0].size <= 0:
+        print("Incorrect value : error in fg indexes")
 
-    def learn_GMMs(self):
-        """Step 2 in Figure 3: Learn GMM parameters from data z"""
-        self.bgd_gmm.fit(self.img[self.bgd_indexes],
-                         self.comp_idxs[self.bgd_indexes])
-        self.fgd_gmm.fit(self.img[self.fgd_indexes],
-                         self.comp_idxs[self.fgd_indexes])
+    return bgd_indexes, fgd_indexes
 
-    def construct_gc_graph(self):
-        bgd_indexes = np.where(self.mask.reshape(-1) == MASK_VALUE['bg'])
-        fgd_indexes = np.where(self.mask.reshape(-1) == MASK_VALUE['fg'])
-        pr_indexes = np.where(np.logical_or(
-            self.mask.reshape(-1) == MASK_VALUE['pr_bg'], self.mask.reshape(-1) == MASK_VALUE['pr_fg']))
+def make_empty_array(r, c):
+    return np.empty((r, c))
 
-        print('bgd count: %d, fgd count: %d, uncertain count: %d' % (
-            len(bgd_indexes[0]), len(fgd_indexes[0]), len(pr_indexes[0])))
 
-        edges = []
-        self.gc_graph_capacity = []
+def calc_beta_smoothness(img, rows, cols, smoothness):
+    # @VEDANSH PLEASE CORRECT THESE 4 LINES
+    _left_diff = img[:, 1:] - img[:, :-1]
+    _up_diff = img[1:, :] - img[:-1, :]
+    _upleft_diff = img[1:, 1:] - img[:-1, :-1]
+    _upright_diff = img[1:, :-1] - img[:-1, 1:]
 
-        # t-links
-        edges.extend(
-            list(zip([self.gc_source] * pr_indexes[0].size, pr_indexes[0])))
-        _D = -np.log(self.bgd_gmm.prob(self.img.reshape(-1, 3)[pr_indexes]))
-        self.gc_graph_capacity.extend(_D.tolist())
-        assert len(edges) == len(self.gc_graph_capacity)
+    sum1 = np.sum(np.square(_left_diff))
+    sum2 = np.sum(np.square(_up_diff))
+    sum3 = np.sum(np.square(_upleft_diff))
+    sum4 = np.sum(np.square(_upright_diff))
+    Σ = sum1+sum2+sum3+sum4
+    global beta
+    global gamma
+    beta = 1 / (2 * Σ / (4 * cols * rows - 3 * (cols + rows) + 2))
+    print(" Beta : ", beta)
+    # Each pixel has 4 neighbors (left, upleft, up, upright)
+    # The 1st column doesn't have left, upleft and the last column doesn't have upright
+    # The first row doesn't have upleft, up and upright
+    # The first and last pixels in the 1st row are removed twice
 
-        edges.extend(
-            list(zip([self.gc_sink] * pr_indexes[0].size, pr_indexes[0])))
-        _D = -np.log(self.fgd_gmm.prob(self.img.reshape(-1, 3)[pr_indexes]))
-        self.gc_graph_capacity.extend(_D.tolist())
-        assert len(edges) == len(self.gc_graph_capacity)
+    #now we have used fomula 11 of the research paper for V (smoothness)
+    smoothness['left_V'] = gamma * np.exp(-beta * np.sum( _left_diff**2, axis=2))
+    smoothness['upleft_V'] = gamma * \
+        np.exp(-beta * np.sum(_upleft_diff** 2, axis=2)) * (1/np.sqrt(2))
+    smoothness['up_V'] = gamma * np.exp(-beta * np.sum(_up_diff** 2, axis=2))
+    smoothness['upright_V'] = gamma * \
+        np.exp(-beta * np.sum(_upright_diff** 2, axis=2)) * (1/np.sqrt(2))
+    return smoothness
 
-        edges.extend(
-            list(zip([self.gc_source] * bgd_indexes[0].size, bgd_indexes[0])))
-        self.gc_graph_capacity.extend([0] * bgd_indexes[0].size)
-        assert len(edges) == len(self.gc_graph_capacity)
 
-        edges.extend(
-            list(zip([self.gc_sink] * bgd_indexes[0].size, bgd_indexes[0])))
-        self.gc_graph_capacity.extend([9 * self.gamma] * bgd_indexes[0].size)
-        assert len(edges) == len(self.gc_graph_capacity)
+def construct_gc_graph(mask,gc,bgd_gmm:GMM,fgd_gmm:GMM,rows,cols,smoothness,img):
+    edges,gc["graph_capacity"]=[],[]
+    mask=mask.reshape(-1) #Edited in mask : NOTE FOR ERROR
+    fg_idx=np.where(mask==MASK_VALUE["fg"])
+    bg_idx=np.where(mask==MASK_VALUE["bg"])
+    pr_idx=np.where(np.logical_or(mask==MASK_VALUE["pr_fg"],mask==MASK_VALUE["pr_bg"]))
 
-        edges.extend(
-            list(zip([self.gc_source] * fgd_indexes[0].size, fgd_indexes[0])))
-        self.gc_graph_capacity.extend([9 * self.gamma] * fgd_indexes[0].size)
-        assert len(edges) == len(self.gc_graph_capacity)
+    print("bg count = ", len(bg_idx[0]), end="")
+    print("fg count = ", len(fg_idx[0]),end='')
+    print("uncertanity count = ", len(pr_idx[0]))
+    global gamma
 
-        edges.extend(
-            list(zip([self.gc_sink] * fgd_indexes[0].size, fgd_indexes[0])))
-        self.gc_graph_capacity.extend([0] * fgd_indexes[0].size)
-        assert len(edges) == len(self.gc_graph_capacity)
+    #T-LINKS
 
-        # print(len(edges))
+    edges.extend(
+        list(zip([gc["source"]] * pr_idx[0].size, pr_idx[0])))
+    _D = -np.log(bgd_gmm.prob(img.reshape(-1, 3)[pr_idx]))
+    gc["graph_capacity"].extend(_D.tolist())
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in adding edges ")
 
-        # n-links
-        img_indexes = np.arange(self.rows * self.cols,
-                                dtype=np.uint32).reshape(self.rows, self.cols)
 
-        mask1 = img_indexes[:, 1:].reshape(-1)
-        mask2 = img_indexes[:, :-1].reshape(-1)
-        edges.extend(list(zip(mask1, mask2)))
-        self.gc_graph_capacity.extend(self.left_V.reshape(-1).tolist())
-        assert len(edges) == len(self.gc_graph_capacity)
+    edges.extend(
+        list(zip([gc["sink"]] * pr_idx[0].size, pr_idx[0])))
+    _D = -np.log(fgd_gmm.prob(img.reshape(-1, 3)[pr_idx]))
+    gc["graph_capacity"].extend(_D.tolist())
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in adding edges ")
 
-        mask1 = img_indexes[1:, 1:].reshape(-1)
-        mask2 = img_indexes[:-1, :-1].reshape(-1)
-        edges.extend(list(zip(mask1, mask2)))
-        self.gc_graph_capacity.extend(
-            self.upleft_V.reshape(-1).tolist())
-        assert len(edges) == len(self.gc_graph_capacity)
 
-        mask1 = img_indexes[1:, :].reshape(-1)
-        mask2 = img_indexes[:-1, :].reshape(-1)
-        edges.extend(list(zip(mask1, mask2)))
-        self.gc_graph_capacity.extend(self.up_V.reshape(-1).tolist())
-        assert len(edges) == len(self.gc_graph_capacity)
+    edges.extend(
+        list(zip([gc["source"]] * bg_idx[0].size, bg_idx[0])))
+    gc["graph_capacity"].extend([0] * bg_idx[0].size)
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in adding edges ")
 
-        mask1 = img_indexes[1:, :-1].reshape(-1)
-        mask2 = img_indexes[:-1, 1:].reshape(-1)
-        edges.extend(list(zip(mask1, mask2)))
-        self.gc_graph_capacity.extend(
-            self.upright_V.reshape(-1).tolist())
-        assert len(edges) == len(self.gc_graph_capacity)
+    edges.extend(
+        list(zip([gc["sink"]] * bg_idx[0].size, bg_idx[0])))
+    gc["graph_capacity"].extend([9 * gamma] * bg_idx[0].size)
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in adding edges ")
 
-        assert len(edges) == 4 * self.cols * self.rows - 3 * (self.cols + self.rows) + 2 + \
-            2 * self.cols * self.rows
+    edges.extend(
+        list(zip([gc["source"]] * fg_idx[0].size, fg_idx[0])))
+    gc["graph_capacity"].extend([9 * gamma] * fg_idx[0].size)
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in adding edges ")
 
-        self.gc_graph = ig.Graph(self.cols * self.rows + 2)
-        self.gc_graph.add_edges(edges)
+    edges.extend(
+        list(zip([gc["sink"]] * fg_idx[0].size, fg_idx[0])))
+    gc["graph_capacity"].extend([0] * fg_idx[0].size)
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in adding edges ")
+    # print(len(edges))
+    # N-LINKS
+    img_index=np.arange(rows*cols).astype('uint32')
+    img_index=img_index.reshape(rows,cols)
 
-    def estimate_segmentation(self):
-        """Step 3 in Figure 3: Estimate segmentation"""
-        mincut = self.gc_graph.st_mincut(
-            self.gc_source, self.gc_sink, self.gc_graph_capacity)
-        print('foreground pixels: %d, background pixels: %d' % (
-            len(mincut.partition[0]), len(mincut.partition[1])))
-        pr_indexes = np.where(np.logical_or(
-            self.mask == MASK_VALUE['pr_bg'], self.mask == MASK_VALUE['pr_fg']))
-        img_indexes = np.arange(self.rows * self.cols,
-                                dtype=np.uint32).reshape(self.rows, self.cols)
-        self.mask[pr_indexes] = np.where(np.isin(img_indexes[pr_indexes], mincut.partition[0]),
-                                         MASK_VALUE['pr_fg'], MASK_VALUE['pr_bg'])
-        self.classify_pixels()
+    mask1 = img_index[:, 1:].reshape(-1)
+    mask2 = img_index[:, :-1].reshape(-1)
+    edges.extend(list(zip(mask1, mask2)))
+    gc["graph_capacity"].extend(smoothness["left_V"].reshape(-1).tolist())
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in N-Links ")
 
-    def calc_energy(self):
-        U = 0
-        for ci in range(self.gmm_components):
-            idx = np.where(np.logical_and(self.comp_idxs == ci, np.logical_or(
-                self.mask == MASK_VALUE['bg'], self.mask == MASK_VALUE['pr_bg'])))
-            U += np.sum(-np.log(self.bgd_gmm.coefs[ci] * self.bgd_gmm.calc_score(self.img[idx], ci)))
+    mask1 = img_index[1:, 1:].reshape(-1)
+    mask2 = img_index[:-1, :-1].reshape(-1)
+    edges.extend(list(zip(mask1, mask2)))
+    gc["graph_capacity"].extend(smoothness["upleft_V"].reshape(-1).tolist())
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in N-Links ")
 
-            idx = np.where(np.logical_and(self.comp_idxs == ci, np.logical_or(
-                self.mask == MASK_VALUE['fg'], self.mask == MASK_VALUE['pr_fg'])))
-            U += np.sum(-np.log(self.fgd_gmm.coefs[ci] * self.fgd_gmm.calc_score(self.img[idx], ci)))
+    mask1 = img_index[1:, :].reshape(-1)
+    mask2 = img_index[:-1, :].reshape(-1)
+    edges.extend(list(zip(mask1, mask2)))
+    gc["graph_capacity"].extend(smoothness["up_V"].reshape(-1).tolist())
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in N-Links ")
 
-        V = 0
-        mask = self.mask.copy()
-        mask[np.where(mask == MASK_VALUE['pr_bg'])] = MASK_VALUE['bg']
-        mask[np.where(mask == MASK_VALUE['pr_fg'])] = MASK_VALUE['fg']
+    mask1 = img_index[1:, :-1].reshape(-1)
+    mask2 = img_index[:-1, 1:].reshape(-1)
+    edges.extend(list(zip(mask1, mask2)))
+    gc["graph_capacity"].extend(smoothness["upright_V"].reshape(-1).tolist())
+    if len(edges)!=len(gc["graph_capacity"]):
+        print("Error in N-Links ")
 
-        V += np.sum(self.left_V * (mask[:, 1:] == mask[:, :-1]))
-        V += np.sum(self.upleft_V * (mask[1:, 1:] == mask[:-1, :-1]))
-        V += np.sum(self.up_V * (mask[1:, :] == mask[:-1, :]))
-        V += np.sum(self.upright_V * (mask[1:, :-1] == mask[:-1, 1:]))
-        return U, V, U + V
+    cap=4*(rows*cols)-3*(rows+cols)+2*(rows*cols)+2
+    assert len(edges) == cap
 
-    def run(self, num_iters=1, skip_learn_GMMs=False):
-        print('skip learn GMMs:', skip_learn_GMMs)
-        for _ in range(num_iters):
-            if not skip_learn_GMMs:
-                self.assign_GMMs_components()
-                self.learn_GMMs()
-            self.construct_gc_graph()
-            self.estimate_segmentation()
-            skip_learn_GMMs = False
-        # print("inrun",self.mask.sum(),self.mask.std())
+    gc["graph"] = ig.Graph(rows*cols+2)
+    gc["graph"].add_edges(edges)
 
-            # print('data term: %f, smoothness term: %f, total energy: %f' % self.calc_energy())
+    # ESTIMATING SEGMENTATION -> STEP 3
+    # This is basically the 3rd part of figure 3
+
+    #we are calculating the mincut 
+    
+    mincut=gc["graph"].st_mincut(gc["source"],gc["sink"],gc["graph_capacity"])
+    print("bg pixels = ", len(mincut.partition[0]), end="")
+    print("fg pixels = ", len(mincut.partition[1]))
+    mask=mask.reshape(rows,cols)
+    pr_idx = (mask == MASK_VALUE['pr_fg']) | (mask == MASK_VALUE['pr_bg'])
+    print("shapes ",pr_idx.shape,mask.shape)
+    img_index = np.arange(rows * cols).astype("uint32")
+    img_index=img_index.reshape(rows, cols)
+    print(img_index[pr_idx].shape)
+    print("mask pr " ,mask[pr_idx].shape)
+    print(np.isin(img_index[pr_idx], mincut.partition[0]).shape)
+    # mask[np.isin(img_index[pr_idx], mincut.partition[0])]=MASK_VALUE['pr_fg']
+    # mask[np.isin(img_index[pr_idx], mincut.partition[1])]=MASK_VALUE['pr_bg']
+    mask[pr_idx] = np.where(np.isin(img_index[pr_idx], mincut.partition[0]),MASK_VALUE['pr_fg'], MASK_VALUE['pr_bg'])
+    
+    classify_pixel(mask)
+
+    updated_variables={
+        "mask":mask,
+        "gc":gc,
+        "bgd_gmm":bgd_gmm,
+        "fgd_gmm":fgd_gmm,
+        "rows":rows,
+        "cols":cols,
+        "smoothness":smoothness,
+        "img":img
+    }
+    return updated_variables
--- run.py
+++ run.py
@@ -5,8 +5,8 @@ def grabcut_fn(img,mask,rect,tmp1,tmp2,itr,mode):
     if mode==1:
         rect=None
     
-    gc=GrabCut(img,mask,rect)
-    gc.run(skip_learn_GMMs=(mode==0))
+    GrabCut(img,mask,rect)
+    # gc.run(skip_learn_GMMs=(mode==0))
     # mask[:]=gc.mask
     # print("ingcu",mask.sum(),mask.std())
 
